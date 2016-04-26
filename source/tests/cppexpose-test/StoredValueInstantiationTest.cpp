7dee3e03a3691ee5b71a633a5ed9271b779e8a97
Florian Wagner <florian.wagner@student.hpi.de>
2016-04-26 16:35:15 +0200

    A basic instantiation test for StoredValue<T>

---
+++ source/tests/cppexpose-test/StoredValueInstantiationTest.cpp
@@ -0,0 +1,43 @@
+
+#include <gmock/gmock.h>
+
+#include <cppexpose/typed/StoredValue.h>
+#include <cppexpose/typed/DirectValue.h>
+#include <utility>
+
+#include "MyObject.h"
+
+using namespace cppexpose;
+using std::string;
+
+
+template <typename T>
+class StoredValueInstantiation_test : public testing::Test
+{
+public:
+    StoredValueInstantiation_test()
+    {
+    }
+
+protected:
+};
+
+typedef ::testing::Types<bool, float, double, int8_t, uint8_t, int16_t, uint16_t, int32_t, uint32_t, int64_t, uint64_t, string> InstantiationTypes;
+TYPED_TEST_CASE(StoredValueInstantiation_test, InstantiationTypes);
+
+namespace {
+
+}
+
+
+// StoredValue instantiaton (read/write)
+
+TYPED_TEST(StoredValueInstantiation_test, instantiateStoredValue)
+{
+    auto storedVal = new StoredValue<TypeParam>();
+
+    ASSERT_EQ(typeid(typename std::remove_cv<TypeParam>::type), storedVal->type());
+
+    delete storedVal;
+}
+
