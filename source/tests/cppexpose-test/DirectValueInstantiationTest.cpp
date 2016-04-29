f8fbed0cee62f50de7de051592d0cc9e095540b8
Florian Wagner <florian.wagner@student.hpi.de>
2016-04-29 14:38:32 +0200

    cleanup

--- source/tests/cppexpose-test/DirectValueInstantiationTest.cpp
+++ source/tests/cppexpose-test/DirectValueInstantiationTest.cpp
@@ -24,10 +24,6 @@ protected:
 typedef ::testing::Types<bool, float, double, int8_t, uint8_t, int16_t, uint16_t, int32_t, uint32_t, int64_t, uint64_t, string> InstantiationTypes;
 TYPED_TEST_CASE(DirectValueInstantiation_test, InstantiationTypes);
 
-namespace {
-
-}
-
 
 // DirectValue instantiaton (read/write)
 
--- source/tests/cppexpose-test/StoredValueInstantiationTest.cpp
+++ source/tests/cppexpose-test/StoredValueInstantiationTest.cpp
@@ -25,10 +25,6 @@ protected:
 typedef ::testing::Types<bool, float, double, int8_t, uint8_t, int16_t, uint16_t, int32_t, uint32_t, int64_t, uint64_t, string> InstantiationTypes;
 TYPED_TEST_CASE(StoredValueInstantiation_test, InstantiationTypes);
 
-namespace {
-
-}
-
 
 // StoredValue instantiaton (read/write)
 
