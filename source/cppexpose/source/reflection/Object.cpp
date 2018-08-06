6187432f204479cf3ceafcb5af95e153dcc0f84e
Willy Scheibel <willy.scheibel@hpi.de>
2018-08-06 18:30:32 +0200

    Explicit string::split parameters

--- source/cppexpose/source/reflection/Object.cpp
+++ source/cppexpose/source/reflection/Object.cpp
@@ -89,13 +89,13 @@ const AbstractProperty * Object::property(size_t index) const
 
 AbstractProperty * Object::property(const std::string & path)
 {
-    std::vector<std::string> splittedPath = cppassist::string::split(path, g_separator);
+    std::vector<std::string> splittedPath = cppassist::string::split(path, g_separator, true);
     return const_cast<AbstractProperty *>(findProperty(splittedPath));
 }
 
 const AbstractProperty * Object::property(const std::string & path) const
 {
-    std::vector<std::string> splittedPath = cppassist::string::split(path, g_separator);
+    std::vector<std::string> splittedPath = cppassist::string::split(path, g_separator, true);
     return findProperty(splittedPath);
 }
 
