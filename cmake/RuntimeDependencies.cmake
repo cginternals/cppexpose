faac91c08644c58215f4bcfaa611aab43e0e25a7
Daniel Limberger <cgdaniel@outlook.com>
2018-10-18 19:47:48 +0200

    refine installer, add runtime deps for msvc install

--- cmake/RuntimeDependencies.cmake
+++ cmake/RuntimeDependencies.cmake
@@ -13,7 +13,24 @@
 
 if(OPTION_SELF_CONTAINED)
 
-    # Install 3rd-party runtime dependencies into runtime-component
-    # install(FILES ... COMPONENT runtime)
+    if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "MSVC")
+
+        # Install 3rd-party runtime dependencies into runtime-component
+        # install(FILES ... COMPONENT runtime)
+
+        find_file(DLLS_CPPASSIST cppassist.dll)
+        find_file(DLLS_CPPLCOATE cpplocate.dll)
+
+        set(DLLS
+            ${DLLS_CPPASSIST}
+            ${DLLS_CPPLCOATE}
+        )
+        set(PLATFORMS
+            ${DLLS_WIN}
+        )
+        install(FILES ${DLLS} DESTINATION ${INSTALL_BIN} COMPONENT runtime)
+        install(FILES ${PLATFORMS} DESTINATION ${INSTALL_BIN}/platforms COMPONENT runtime)
+
+    endif()
 
 endif()
